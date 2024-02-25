a965d87a72cca05d881c3d2e23e33f7e74c846a8
Jett Hollister <jett.hollister@duke.edu>
2024-02-25 00:23:49 -0800

    Added cron example

---
+++ examples/cron-bot.ts
@@ -0,0 +1,30 @@
+import { BskyAgent } from "@atproto/api";
+import { CronJob } from "cron";
+import * as dotenv from "dotenv";
+import * as process from "process";
+
+dotenv.config();
+
+// Create a Bluesky Agent
+const agent = new BskyAgent({
+  service: "https://bsky.social",
+});
+
+async function main() {
+  await agent.login({
+    identifier: process.env.BLUESKY_USERNAME!,
+    password: process.env.BLUESKY_PASSWORD!,
+  });
+  await agent.post({
+    text: "🙂",
+  });
+  console.log("Just posted!");
+}
+
+// Run this on a cron job
+const scheduleExpressionMinute = "* * * * *"; // Run once every minute for testing
+const scheduleExpression = "0 */3 * * *"; // Run once every three hours in prod
+
+const job = new CronJob(scheduleExpression, main); // change to scheduleExpressionMinute for testing
+
+job.start();
